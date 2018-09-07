6085c5e670ebdcf4fa953cc8dd4ecd22232960cc
Mikko Miranda <MikkoMiranda@Apples-MacBook-Pro.local>
2018-09-06 22:01:08 -0700

    first commit

---
+++ src/components/AboutMe.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class AboutMe extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Contact Here</h1>
+      </div>
+    );
+  }
+}
